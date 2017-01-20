abe4fc8cf47f29fc98146b1456a389cf55d661d9
Couto <cs.couto@outlook.com>
2017-01-20 03:24:52 -0800

    model

---
+++ pokedex/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Tiago henrique on 1/20/17.
+//  Copyright © 2017 Tiago henrique. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Tiago henrique on 1/20/17.
+//  Copyright © 2017 Tiago henrique. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int{
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
