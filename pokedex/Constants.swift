9560ba220f4a814c9008bf2d351e1cb0bf40f9f3
Couto <cs.couto@outlook.com>
2017-01-23 07:06:16 -0800

    alamofire http request

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Tiago henrique on 1/23/17.
+//  Copyright © 2017 Tiago henrique. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -11,11 +11,32 @@ import UIKit
 class PokemonDetailVC: UIViewController {
     
     var pokemon: Pokemon!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var desc: UILabel!
+    @IBOutlet weak var pokeType: UILabel!
+    @IBOutlet weak var pokeHeight: UILabel!
+    @IBOutlet weak var pokeWeight: UILabel!
+    @IBOutlet weak var pokeDef: UILabel!
+    @IBOutlet weak var pokeId: UILabel!
+    @IBOutlet weak var pokeAtck: UILabel!
+    @IBOutlet weak var pokeEvo: UILabel!
+    @IBOutlet weak var evoImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        name.text = pokemon.name
+        
+        pokemon.downloadPokemonDetail{
+            self.updateUI()
+        }
+    }
+    
+    func updateUI(){
+        
+    }
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
 
 }
